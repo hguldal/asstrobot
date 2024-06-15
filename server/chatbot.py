8182fdaad8971e953dd4d7d401c4257b27e7d4c5
Hakan Guldal <hguldal@gmail.com>
2024-06-15 23:58:45 +0300

    openai support added

--- server/chatbot.py
+++ server/chatbot.py
@@ -1,9 +1,21 @@
 import re, string, unicodedata,time,datetime,csv,random,json
 
+from openai import OpenAI
+client = OpenAI()
+
 def chatbot_answer(question):
     
-
     returnAnswer=''
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "AsstroBot"},
+            {"role": "user", "content": question}
+        ]
+    )
+
+    returnAnswer=completion.choices[0].message
     
     csvLogdata = [datetime.datetime.now(), question, returnAnswer]
 
